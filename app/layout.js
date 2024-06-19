26421ed658b84569423e220c17f676da3e3032a9
favorito26 <mufaddal2003@gmail.com>
2024-06-19 14:58:25 +0530

    new commit

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 
@@ -13,10 +11,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/logo.png"/>
+      <link rel="icon" href="/logo.png" type="image/png"/>
         <title>Qutbi Tutorials</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={inter.className}>
+      <body>
         <Navbar/>
         
        <div className="min-h-[90vh]">
